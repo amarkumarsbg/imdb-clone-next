d19f13ad16ab743caf5dfadbc3dae08f5bd65ae5
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-02-18 18:41:08 +0530

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Results from "@/components/Results";
 import React from "react";
+import { resolve } from "styled-jsx/css";
 
 const API_KEY = process.env.API_KEY;
 
@@ -9,7 +10,8 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
 
   if (!res.ok) {
